bdd011851efc914359593683c83f4062e26c29fc
michael0022 <mahbub022@gmail.com>
2018-10-31 13:34:19 +0530

    Add files via upload

---
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/XGBoost/Programs/XGBoost.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 13:58:58 2018
+
+@author: Despacito
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x_1 = LabelEncoder()
+X[:, 1] = labelencoder_x_1.fit_transform(X[:, 1])
+labelencoder_x_2 = LabelEncoder()
+X[:, 2] = labelencoder_x_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting XGBoost to the training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying K-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+mean = accuracies.mean()
+sd = accuracies.std()
+
+
+
+
+
+
+
+
+
+
+
---
+++ Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/LDA/Programs/LinearDiscriminantAnalysis.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 15:34:22 2018
+
+@author: Despacito
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset
+dataSet = pd.read_csv("Wine.csv")
+X = dataSet.iloc[:, 0:13].values
+Y = dataSet.iloc[:, 13].values
+
+
+# splitting dataset into training and test set.
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Applying LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components = 2)
+X_train = lda.fit_transform(X_train, Y_train)
+X_test = lda.transform(X_test)
+
+# Fitting Logistic Regression to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, Y_train)
+
+#Predicting the test case results
+Y_Pred = classifier.predict(X_test)
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_Pred) # It depicts number of correct and wrong predictions
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, Y_train
+
+# This creates the coordinates for graph with higher resolution
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+# This divides the data in red and green region and draws the prediction line, if data belongs to class 0 it colorizes in red and for class 1 it colorrizes in 1.
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+
+# Graph plotting
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+# Now it is scattering the data on the graph
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+    
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+
+# This creates the coordinates for graph with higher resolution
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+# This divides the data in red and green area and draws the prediction line, if data belongs to class 0 it colorizes in red and for class 1 it colorrizes in 1.
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+
+# Graph plotting
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+# Now it is scattering the data on the graph
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+    
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
+
